e5d27bd2b76e5877e268acf9456721754c58084c
chayma-chabchoub <chaabchouubchaiima@gmail.com>
2023-11-21 12:36:51 +0100

    first commit

---
+++ src/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <div>
+      {/* heading */}
+      <h1> this a photo </h1>
+    </div>
+  )
+}
+
+export default AboutMe
---
+++ src/App.js
@@ -0,0 +1,93 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import AboutMe from './AboutMe';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+function App() {
+ 
+//  navbar 
+return (
+    <div className='app'>
+    <Navbar expand="lg" className="bg-body-tertiary">
+    <Container>
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  <AboutMe/>  
+  {/* this a  3 cards  */}
+  <CardGroup>
+    {/* this a card 1  */}
+      <Card>
+        <Card.Img variant="top" src="pic animals.jpeg" />
+        <Card.Body>
+          <Card.Title>Pic animals </Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+       {/* this a card 2  */}
+      <Card>
+        <Card.Img variant="top" src="women.webp" />
+        <Card.Body>
+          <Card.Title>Women</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+         {/* this a card 3  */}
+        <Card.Img variant="top" src="landscape.jpeg" />
+        <Card.Body>
+          <Card.Title>landscapee</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+     </div>
+    
+
+  ) ;
+}
+
+export default App;
